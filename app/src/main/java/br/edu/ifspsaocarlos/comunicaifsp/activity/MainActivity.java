b6c4e4003d31f2e85e3f7397f66cee49817cf4af
Sgobbe <Ricardo.Sgobbe@vistajet.com>
2017-11-07 21:03:14 -0200

    entrar topico na main funcionando

--- app/src/main/java/br/edu/ifspsaocarlos/comunicaifsp/activity/MainActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/comunicaifsp/activity/MainActivity.java
@@ -8,18 +8,26 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import br.edu.ifspsaocarlos.comunicaifsp.LibraryClass;
 import br.edu.ifspsaocarlos.comunicaifsp.R;
+import br.edu.ifspsaocarlos.comunicaifsp.Topic;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authStateListener;
     private DrawerLayout container;
     private NavigationView navigationView;
+    private RecyclerView mRecycler;
+
+
 
 
     @Override
@@ -37,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         container = (DrawerLayout) findViewById(R.id.main_container);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mRecycler = (RecyclerView) findViewById(R.id.meus_topicos);
         configNavigationView();
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -64,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
+
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -80,6 +93,45 @@ public class MainActivity extends AppCompatActivity {
         firebaseAuth.addAuthStateListener(authStateListener);
         databaseReference = LibraryClass.getFirebase();
         databaseReference.getRef();
+
+
+        mRecycler.setAdapter(new FirebaseRecyclerAdapter<Topic, TopicoActivity.MyViewHolder>(Topic.class, R.layout.cell_topico,
+                TopicoActivity.MyViewHolder.class, databaseReference.child("usuario_topico").child(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+            @Override
+            protected void populateViewHolder(TopicoActivity.MyViewHolder viewHolder, Topic model, int position) {
+                final Topic modelFinal = model;
+                viewHolder.txt_name.setText("[" + model.getCourse().toUpperCase()+ "] " + model.getName());
+                viewHolder.txt_msg.setText(model.getDescription());
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Pode usar o modelFinal aqui
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("usuario_topico_id");
+                        ref.orderByChild("id").equalTo(modelFinal.getIdTopic()).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.exists()){
+                                    Intent intent = new Intent(MainActivity.this, TopicMessageActivity.class);
+                                    intent.putExtra("topic", modelFinal);
+                                    startActivity(intent);
+                                }else{
+                                    Intent intent = new Intent(MainActivity.this, SignInTopicActivity.class);
+                                    intent.putExtra("topic", modelFinal);
+                                    startActivity(intent);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
